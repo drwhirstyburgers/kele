28e4f721b3e5b47568fbef00a4dea31a183526ee
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-06-12 18:39:14 -0400

    gemspec add, auth_token received

---
+++ lib/kele.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    response = self.class.post("/sessions", body: { "email":email, "password":password })
+    @auth_token = response["auth_token"]
+    raise "Invalid Email or Password" if @auth_token.nil?
+  end
+end
