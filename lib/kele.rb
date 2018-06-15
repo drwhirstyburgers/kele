122396a41b88f437945908bf5e18ec073a49c8f0
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-06-15 17:52:38 -0400

    get_messages working

--- lib/kele.rb
+++ lib/kele.rb
@@ -23,4 +23,14 @@ class Kele
     response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
   end
+
+  def get_messages(page_number = 0)
+    if page_number > 0
+      response = self.class.get("/message_threads?page=#{page_number}", headers: { "authorization" => @auth_token })
+      @message_threads = JSON.parse(response.body)
+    else
+      response = self.class.get("/message_threads", headers: { "authorization" => @auth_token })
+      @message_page = JSON.parse(response.body)
+    end
+  end
 end
